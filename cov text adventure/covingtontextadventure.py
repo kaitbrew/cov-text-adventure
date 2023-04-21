ae8714fa68ade112bc30fcd57ce54d013a2c7ffc
kait <kaitlynmbrewer@gmail.com>
2023-04-21 15:20:05 -0400

    first commit

--- cov text adventure/covingtontextadventure.py
+++ cov text adventure/covingtontextadventure.py
@@ -1,5 +1,8 @@
+
+
 #NAME ENTRY
 name = input("What was your name, again? \n>").title()
+
 #NAME CONFIRM
 confname = input(f"Are you sure you want to be called {name}?\n1. Yes\n2. No\n>")
 while confname != "1": 
@@ -11,6 +14,7 @@ while confname != "1":
 if confname == "1":
     print(f"Good morning, {name}.")
 print("Now, we will determine your strengths and weaknesses. There are 3 stats: Strength, Speed and Smarts. Each has its own advantages. Assign the numbers to each attribute according to how much you want your character to be focused in it.\nFor example, if you want a very strong character, you will have to sacrifice on Speed or Smarts, because each value can be used only once. To create a strong character that is lacking in speed, assign Strength to 3, Speed to 1, Smarts to 2.")
+
 #SETTING UP STATS
 def assign_stats():
     values = {}
@@ -28,8 +32,14 @@ def assign_stats():
             else:
                 print('Invalid input. Please use only whole numbers between 1-3.')
     return values
+
+#ASSIGN VARIABLES TO EACH STAT
 stats=assign_stats()
+str=stats["Strength"]
+spe=stats["Speed"]
+sma=stats["Smarts"]
 print(stats)
+
 #CONFIRM STATS
 confstat = input("Is this who you want to be?\n1. Yes\n2. No\n>")
 while confstat != "1": 
@@ -43,4 +53,16 @@ while confstat != "1":
      print(stats)
      confstat=input("Please enter '1' or '2': \nIs this who you want to be?\n1. Yes\n2. No\n>")
 if confstat == "1":
-    print("As it is written, so it shall be done.")
+    print("As it is written, so it shall be done.\n")
+    if str==3:
+       print("Strongboi eh?\n")
+    if spe==3:
+       print("Fast type eh?\n")
+    if sma==3:
+       print("Big brain guy eh?\n")
+
+#INTRO PAGE
+intro=open('intro.txt','r')
+introtxt = intro.read()
+print(introtxt)
+intro.close
