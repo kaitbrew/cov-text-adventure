dd08441ed7be6262752158b3ea9989205032d644
kait <kaitlynmbrewer@gmail.com>
2023-04-24 15:08:06 -0400

    updated text print style

--- cov text adventure/block editor.py
+++ cov text adventure/block editor.py
@@ -1,46 +1,9 @@
-#SETTING UP STATS
-def assign_stats():
-    values = {}
-    print("Please assign values 1-3 to the following attributes:")
-    for attribute in ["Strength", "Speed", "Smarts"]:
-        while True:
-            value = input(f"{attribute.capitalize()}:\n>")
-            if value.isdigit() and 1 <= int(value) <= 3 and int(value) not in values.values():
-                values[attribute] = int(value)
-                break
-            elif value.isdigit() and 1 <= int(value) <= 3 and int(value) in values.values():
-                print("Invalid input. Please enter a number that hasn't been used before.")
-            elif value.isalpha():
-                print('Invalid input. Please use only whole numbers between 1-3.')
-            else:
-                print('Invalid input. Please use only whole numbers between 1-3.')
-    return values
+import time
 
-#ASSIGN VARIABLES TO EACH STAT
-stats=assign_stats()
-str=stats["Strength"]
-spe=stats["Speed"]
-sma=stats["Smarts"]
-print(stats)
+filename = "intro.txt"
+with open(filename, "r") as file:
+    text = file.read()
 
-#CONFIRM STATS
-confstat = input("Is this who you want to be?\n1. Yes\n2. No\n>")
-while confstat != "1": 
-    if confstat == "2":
-     print("Let's try again!")
-     stats=assign_stats()
-     print(stats)
-     confstat=input("Is this who you want to be?\n1. Yes\n2. No\n>")
-    else:
-     print("Invalid input.")
-     print(stats)
-     confstat=input("Please enter '1' or '2': \nIs this who you want to be?\n1. Yes\n2. No\n>")
-
-if confstat == "1":
-    print("As it is written, so it shall be done.")
-    if str==3:
-       print("You treat your body, and in return, you expect it to treat you. Long hours in the gym mean that you can lift more, run faster, jump higher.")
-    if spe==3:
-       print("Fast type eh?\n")
-    if sma==3:
-       print("Big brain guy eh?\n")
+for char in text:
+    print(char, end='', flush=True)
+    time.sleep(0.05)
--- cov text adventure/covingtontextadventure.py
+++ cov text adventure/covingtontextadventure.py
@@ -1,42 +1,56 @@
-
+#SLOW TEXT EFFECT
+import time
+def prints(text, delay=0.01):
+    for char in text:
+        if char == '\n':
+            print()
+        else:
+            print(char, end='', flush=True)
+            time.sleep(delay)
 
 #NAME ENTRY
-name = input("What was your name, again? \n>").title()
+prints('What was your name, again?')
+name = input("\n>").title()
 
 #NAME CONFIRM
-confname = input(f"Are you sure you want to be called {name}?\n1. Yes\n2. No\n>")
+prints(f"Are you sure you want to be called {name}?\n1. Yes\n2. No")
+confname = input("\n>")
 while confname != "1": 
     if confname == "2":
-     name = input("Okay, please tell me your name: \n>").title()
-     confname = input(f"Are you sure you want to be called {name}?\n1. Yes\n2. No\n>")
+     prints("Okay, please tell me your name: ")
+     name = input("\n>").title()
+     prints(f"Are you sure you want to be called {name}?\n1. Yes\n2. No")
+     confname = input("\n>")
     else:
-     confname = input(f"Invalid input. Please enter '1' or '2': \nAre you sure you want to be called {name}?\n1. Yes\n2. No\n>")
+     prints(f"Invalid input. Please enter '1' or '2': \nAre you sure you want to be called {name}?\n1. Yes\n2. No")
+     confname = input("\n>")
 if confname == "1":
-    print(f"Good morning, {name}.")
-print("Now, we will determine your strengths and weaknesses. There are 3 stats: Strength, Speed and Smarts. Each has its own advantages. Assign the numbers to each attribute according to how much you want your character to be focused in it.\nFor example, if you want a very strong character, you will have to sacrifice on Speed or Smarts, because each value can be used only once. To create a strong character that is lacking in speed, assign Strength to 3, Speed to 1, Smarts to 2.")
+    prints(f"Good morning, {name}.\n")
+prints("Now we will determine your strengths and weaknesses. There are 3 stats: Strength, Speed and Smarts. Each has its own advantages. Assign the numbers to each attribute according to how much you want your character to be focused in it.\nFor example, if you want a very strong character, you will have to sacrifice on Speed or Smarts, because each value can be used only once. To create a strong character that is lacking in speed, assign Strength to 3, Speed to 1, Smarts to 2.\n")
 
 #SETTING UP STATS
 def assign_stats():
     values = {}
-    print("Please assign values 1-3 to the following attributes:")
+    prints("Please assign values 1-3 to the following attributes: \n")
     for attribute in ["Strength", "Speed", "Smarts"]:
         while True:
-            value = input(f"{attribute.capitalize()}:\n>")
+            prints(f"\n{attribute.capitalize()}:")
+            value = input("\n>")
             if value.isdigit() and 1 <= int(value) <= 3 and int(value) not in values.values():
                 values[attribute] = int(value)
                 break
             elif value.isdigit() and 1 <= int(value) <= 3 and int(value) in values.values():
-                print("Invalid input. Please enter a number that hasn't been used before.")
+                prints("Invalid input. Please enter a number that hasn't been used before.\n")
             elif value.isalpha():
-                print('Invalid input. Please use only whole numbers between 1-3.')
+                prints('Invalid input. Please use only whole numbers between 1-3.\n')
             else:
-                print('Invalid input. Please use only whole numbers between 1-3.')
+                prints('Invalid input. Please use only whole numbers between 1-3.\n')
     return values
 
 #SETTING UP STATS
 def assign_stats():
     values = {}
-    print("Please assign values 1-3 to the following attributes:")
+    prints("Please assign values 1-3 to the following attributes:\n")
     for attribute in ["Strength", "Speed", "Smarts"]:
         while True:
             value = input(f"{attribute.capitalize()}:\n>")
@@ -44,44 +58,48 @@ def assign_stats():
                 values[attribute] = int(value)
                 break
             elif value.isdigit() and 1 <= int(value) <= 3 and int(value) in values.values():
-                print("Invalid input. Please enter a number that hasn't been used before.")
+                prints("Invalid input. Please enter a number that hasn't been used before.\n")
             elif value.isalpha():
-                print('Invalid input. Please use only whole numbers between 1-3.')
+                prints('Invalid input. Please use only whole numbers between 1-3.\n')
             else:
-                print('Invalid input. Please use only whole numbers between 1-3.')
+                prints('Invalid input. Please use only whole numbers between 1-3.\n')
     return values
 
 #ASSIGN VARIABLES TO EACH STAT
 stats=assign_stats()
-str=stats["Strength"]
-spe=stats["Speed"]
-sma=stats["Smarts"]
 print(stats)
 
 #CONFIRM STATS
 confstat = input("Is this who you want to be?\n1. Yes\n2. No\n>")
 while confstat != "1": 
     if confstat == "2":
-     print("Let's try again!")
+     prints("Let's try again!\n")
      stats=assign_stats()
      print(stats)
      confstat=input("Is this who you want to be?\n1. Yes\n2. No\n>")
     else:
-     print("Invalid input.")
+     prints("Invalid input.\n")
      print(stats)
      confstat=input("Please enter '1' or '2': \nIs this who you want to be?\n1. Yes\n2. No\n>")
-
+str=stats["Strength"]
+spe=stats["Speed"]
+sma=stats["Smarts"]
 if confstat == "1":
-    print("As it is written, so it shall be done.")
+    prints("As it is written, so it shall be done.\n")
     if str==3:
-       print("You treat your body, and in return, you expect it to treat you. Long hours in the gym mean that you can lift more, run faster, jump higher.")
+       prints("You treat your body, and in return, you expect it to treat you. Long hours in the gym mean that you can lift more, run faster, jump higher.\n")
     if spe==3:
-       print("Fast type eh?\n")
+       prints("\n")
     if sma==3:
-       print("Big brain guy eh?\n")
+       prints("Big brain guy eh?\n")
+
+#PRINT 3 PERIODS FOR LOADING
+for i in range(3):
+    print(".", end="", flush=True)
+    time.sleep(2)
 
 #INTRO PAGE
 intro=open('intro.txt','r')
 introtxt = intro.read()
-print(introtxt)
+prints(f"\n"+introtxt)
 intro.close
