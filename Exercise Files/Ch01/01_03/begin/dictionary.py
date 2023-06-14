fbad5fe1c7c99189db93df5523ebe344ffadb9b4
kait <kaitlynmbrewer@gmail.com>
2023-06-14 10:12:57 -0400

    cleaning this up for potential employers

--- Exercise Files/Ch01/01_03/begin/dictionary.py
+++
@@ -1,5 +0,0 @@
-# California state symbols
-state_bird = 'California quail'
-state_animal = 'Grizzly bear'
-state_flower = 'California poppy'
-state_fruit = 'Avocado'
--- Exercise Files/Ch02/02_03/begin/while_loop.py
+++
@@ -1,6 +0,0 @@
-i=5
-print("counting to 100 by 5s")
-while i<=100:
-    print(i)
-    i+=5
-print("list complete")
